8a415204069e13d2b2c620dbe37ca160d35e28bf
Aspectz <josepgarzo7@gmail.com>
2020-11-02 00:13:45 +0100

    ProyectoDOMFinished

--- ProyectoDOM/src/modelo/ProcedimientoMateria.java
+++ ProyectoDOM/src/modelo/ProcedimientoMateria.java
@@ -3,13 +3,25 @@ package modelo;
 import java.util.ArrayList;
 
 public class ProcedimientoMateria {
-	private int id;
+	private String id;
 	private String title;
-	private ArrayList<ProcedimientoTramite> procedimientos;
-	public int getId() {
+	private ArrayList<ProcedimientoTramite> procedimientos=new ArrayList<ProcedimientoTramite>();
+	
+	
+	
+	public ProcedimientoMateria(String id, String title,String id1,String title1,String enLinea,String certificado) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.procedimientos.add(new ProcedimientoTramite(id1, title1, enLinea, certificado));
+	}
+	public ProcedimientoMateria() {
+		
+	}
+	public String getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public String getTitle() {
--- ProyectoDOM/src/vista/App.java
+++ ProyectoDOM/src/vista/App.java
@@ -1,10 +1,33 @@
 package vista;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+
+import controlador.ProcedimientoMateriaAgrupacionControllador;
+import modelo.MateriasTipo;
+import modelo.ProcedimientoMateriaAgrupacion;
+
+
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
 		// TODO Auto-generated method stub
-			
+		String nomFich="zaragozaxml.xml";
+		File f1=new File(nomFich);
+		Document doc=null;
+		ProcedimientoMateriaAgrupacionControllador procedimientoMateriaAgrupacion=new ProcedimientoMateriaAgrupacionControllador(f1);
+		doc=procedimientoMateriaAgrupacion.recuperarXML();
+		
+		ProcedimientoMateriaAgrupacion materiasTipo=null;
+		materiasTipo=procedimientoMateriaAgrupacion.leerDOM();
+		
+		System.out.println(materiasTipo);
 	}
 
 }
