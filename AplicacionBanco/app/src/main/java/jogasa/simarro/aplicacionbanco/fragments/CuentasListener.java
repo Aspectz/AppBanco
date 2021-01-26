20a3db89014e666beaa5af5b7e8866b47032354f
Aspectz <61050550+Aspectz@users.noreply.github.com>
2021-01-26 23:50:31 +0100

    Delete AplicacionBanco directory

--- AplicacionBanco/app/src/main/java/jogasa/simarro/aplicacionbanco/fragments/CuentasListener.java
+++
@@ -1,7 +0,0 @@
-package jogasa.simarro.aplicacionbanco.fragments;
-
-import jogasa.simarro.aplicacionbanco.pojo.Cuenta;
-
-public interface CuentasListener {
-    void onCuentaSeleccionada(Cuenta c);
-}
